dd87a9eee567b3d58b59f1c94c1c186e189ec32c
Nikhil Ratta <nikhilratta84@gmail.com>
2021-07-04 11:32:00 +0530

    Created the page to add subscribers

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Header from './Header';
-import './common/common.css';
+import './AddSubscriber.css';
 
 class AddSubscriber extends Component{
   render(){
@@ -8,6 +8,17 @@ class AddSubscriber extends Component{
       <div>
           <Header heading="Add Subscriber"/>
           <button className="btn">Back</button>
+          <form className="subscriber-form">
+           <label htmlFor="name" className="label-control"> Name:</label><br/>
+           <input className="input-control" name="name" id="name"></input><br/>
+           <label htmlFor="phone" className="label-control"> Phone:</label><br/>
+           <input className="input-control" name="phone" id="phone"></input><br/>
+           <div className="subscriber-info-container">
+             <span className="subscriber-to-add-heading">Subscriber to be added:</span><br/>
+             <span className="subscriber-info">Name: </span><br/>
+             <span className="subscriber-info">Phone: </span><br/>
+          </div>
+          </form>
       </div>
     )
   }
